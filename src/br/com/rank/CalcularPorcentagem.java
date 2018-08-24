0cda595a573da97bcfbf6378e709f44649e5effe
Samuel Farias <samuellima810@gmail.com>
2018-08-24 09:52:58 -0300

    Algumas modificações

--- src/br/com/rank/CalcularPorcentagem.java
+++ src/br/com/rank/CalcularPorcentagem.java
@@ -6,6 +6,11 @@ public class CalcularPorcentagem {
 	
 	private EmpresaA myBank;
 	
+	private Integer[] favoravel;
+	private Integer[] neutro;
+	private Integer[] desfavoravel;
+	private Integer[] descartavel;
+	
 	public CalcularPorcentagem() {}
 	
 	public CalcularPorcentagem(EmpresaA bank) {
@@ -20,20 +25,118 @@ public class CalcularPorcentagem {
 		for(int i=0; i<tamanho; i++) {
 			if(clientes.get(i).equals(4567)) {
 				if(avaliacoes.get(i) == 0 || avaliacoes.get(i) == 1) {
-					System.out.println("Favoravel");
+					this.favoravel[i] = avaliacoes.get(i);
 				}
 				else if(avaliacoes.get(i) == 2) {
-					System.out.println("Neutro");
+					this.neutro[i] = avaliacoes.get(i);
 				}
 				else if(avaliacoes.get(i) == 3 || avaliacoes.get(i) == 4) {
-					System.out.println("Desfavoravel");
+					this.desfavoravel[i] = avaliacoes.get(i);
 				}
 				else {
-					System.out.println("Valor descartado");
+					this.descartavel[i] = avaliacoes.get(i);
+					//System.out.println("Valor descartado");
 				}
 			}
 		}
 		
+		
+		for(int i=0; i<tamanho; i++) {
+			if(clientes.get(i).equals(4568)) {
+				if(avaliacoes.get(i) == 0 || avaliacoes.get(i) == 1) {
+					this.favoravel[i] = avaliacoes.get(i);
+				}
+				else if(avaliacoes.get(i) == 2) {
+					this.neutro[i] = avaliacoes.get(i);
+				}
+				else if(avaliacoes.get(i) == 3 || avaliacoes.get(i) == 4) {
+					this.desfavoravel[i] = avaliacoes.get(i);
+				}
+				else {
+					this.descartavel[i] = avaliacoes.get(i);
+					//System.out.println("Valor descartado");
+				}
+			}
+		}
+		
+		
+		for(int i=0; i<tamanho; i++) {
+			if(clientes.get(i).equals(4569)) {
+				if(avaliacoes.get(i) == 0 || avaliacoes.get(i) == 1) {
+					this.favoravel[i] = avaliacoes.get(i);
+				}
+				else if(avaliacoes.get(i) == 2) {
+					this.neutro[i] = avaliacoes.get(i);
+				}
+				else if(avaliacoes.get(i) == 3 || avaliacoes.get(i) == 4) {
+					this.desfavoravel[i] = avaliacoes.get(i);
+				}
+				else {
+					this.descartavel[i] = avaliacoes.get(i);
+					//System.out.println("Valor descartado");
+				}
+			}
+		}
+		
+		
+		for(int i=0; i<tamanho; i++) {
+			if(clientes.get(i).equals(4570)) {
+				if(avaliacoes.get(i) == 0 || avaliacoes.get(i) == 1) {
+					this.favoravel[i] = avaliacoes.get(i);
+				}
+				else if(avaliacoes.get(i) == 2) {
+					this.neutro[i] = avaliacoes.get(i);
+				}
+				else if(avaliacoes.get(i) == 3 || avaliacoes.get(i) == 4) {
+					this.desfavoravel[i] = avaliacoes.get(i);
+				}
+				else {
+					this.descartavel[i] = avaliacoes.get(i);
+					//System.out.println("Valor descartado");
+				}
+			}
+		}
+		
+		
+		for(int i=0; i<tamanho; i++) {
+			if(clientes.get(i).equals(4571)) {
+				if(avaliacoes.get(i) == 0 || avaliacoes.get(i) == 1) {
+					this.favoravel[i] = avaliacoes.get(i);
+				}
+				else if(avaliacoes.get(i) == 2) {
+					this.neutro[i] = avaliacoes.get(i);
+				}
+				else if(avaliacoes.get(i) == 3 || avaliacoes.get(i) == 4) {
+					this.desfavoravel[i] = avaliacoes.get(i);
+				}
+				else {
+					this.descartavel[i] = avaliacoes.get(i);
+					//System.out.println("Valor descartado");
+				}
+			}
+		}
+		
+	}
+	
+	
+	public void imprimirValor() {
+		Integer[] fav = null;
+		Integer[] desf = null;
+		
+		
+		for(int i=0; i<this.favoravel.length; i++) {
+			fav[i] = this.favoravel[i];
+			System.out.println("Valor: " + fav[i]);
+		}
+		
+		for(int i=0; i<this.favoravel.length; i++) {
+			desf[i] = this.desfavoravel[i];
+			System.out.println("Valor: " + desf[i]);
+		}
+		
+		
+		
+		System.out.println();
 	}
 
 
@@ -44,6 +147,38 @@ public class CalcularPorcentagem {
 
 	public void setMyBank(EmpresaA myBank) {
 		this.myBank = myBank;
+	}
+
+	public Integer[] getFavoravel() {
+		return favoravel;
+	}
+
+	public void setFavoravel(Integer[] favoravel) {
+		this.favoravel = favoravel;
+	}
+
+	public Integer[] getNeutro() {
+		return neutro;
+	}
+
+	public void setNeutro(Integer[] neutro) {
+		this.neutro = neutro;
+	}
+
+	public Integer[] getDesfavoravel() {
+		return desfavoravel;
+	}
+
+	public void setDesfavoravel(Integer[] desfavoravel) {
+		this.desfavoravel = desfavoravel;
+	}
+
+	public Integer[] getDescartavel() {
+		return descartavel;
+	}
+
+	public void setDescartavel(Integer[] descartavel) {
+		this.descartavel = descartavel;
 	}
 
 }
